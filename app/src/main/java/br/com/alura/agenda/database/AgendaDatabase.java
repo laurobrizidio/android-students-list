5189db21b9a8e22c5769a11c3bfabc535eed9cfe
Lauro Brizidio <lauro.brizidio@itbam.org.br>
2020-08-05 11:57:21 -0400

    refactor: Migrations

--- app/src/main/java/br/com/alura/agenda/database/AgendaDatabase.java
+++ app/src/main/java/br/com/alura/agenda/database/AgendaDatabase.java
@@ -14,44 +14,21 @@ import br.com.alura.agenda.database.converter.ConversorCalendar;
 import br.com.alura.agenda.database.dao.AlunoDAO;
 import br.com.alura.agenda.model.Aluno;
 
+import static br.com.alura.agenda.database.AgendaMigrations.ALL_MIGRATIONS;
+
 @Database(entities = {Aluno.class},version = 4,exportSchema = false)
 @TypeConverters({ConversorCalendar.class})
 public abstract class AgendaDatabase extends RoomDatabase {
 
     public static final String AGENDA_DB = "agenda.db";
 
+
     public abstract AlunoDAO getRoomAlunoDAO();
 
     public static AgendaDatabase getInstance(Context context){
-        return Room.databaseBuilder(context,AgendaDatabase.class, AGENDA_DB)
+        return Room.databaseBuilder(context, AgendaDatabase.class, AGENDA_DB)
                 .allowMainThreadQueries()
-                .addMigrations(new Migration(1, 2) {
-                                   @Override
-                                   public void migrate(@NonNull SupportSQLiteDatabase database) {
-                                       database.execSQL("ALTER TABLE aluno ADD COLUMN sobrenome TEXT");
-                                   }
-                               }, new Migration(2, 3) {
-                                   @Override
-                                   public void migrate(@NonNull SupportSQLiteDatabase database) {
-                                       database.execSQL("CREATE TABLE IF NOT EXISTS `Aluno_novo` " +
-                                               "(`id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
-                                               "`nome` TEXT, " +
-                                               "`telefone` TEXT, `email` TEXT)");
-
-                                       database.execSQL("INSERT INTO Aluno_novo (id,nome,telefone,email) " +
-                                               "SELECT id,nome,telefone,email FROM Aluno");
-
-                                       database.execSQL("DROP TABLE Aluno");
-
-                                       database.execSQL("ALTER TABLE Aluno_novo RENAME TO Aluno");
-                                   }
-                               }, new Migration(3,4) {
-                                   @Override
-                                   public void migrate(@NonNull SupportSQLiteDatabase database) {
-                                       database.execSQL("ALTER TABLE aluno ADD COLUMN momentoDeCadastro INTEGER");
-                                   }
-                               }
-                )
+                .addMigrations(ALL_MIGRATIONS)
                 .build();
     }
 
